7a8d685530f632a711a555cba37a3d3236770c31
Amine.Mraihi <mraihiamin@gmail.com>
2022-01-26 16:39:03 +0100

    starting point

---
+++ src/main/java/com/cadreConget/cadreConget/CadreCongetApplication.java
@@ -0,0 +1,13 @@
+package com.cadreConget.cadreConget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadreCongetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadreCongetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadreConget/cadreConget/CadreCongetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadreConget.cadreConget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadreCongetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
